00e5b665e4e615c46b041bcf46572cfc802a4a37
Lauren Minchin <9057038+lfvr@users.noreply.github.com>
2023-05-17 20:37:33 +0000

    Fix tests

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -6,9 +6,9 @@ import geopandas as gpd
 import movingpandas as mpd
 import pandas as pd
 from shapely.geometry import Point
+from tests.config.definitions import ROOT_DIR
 from app.app import App
 from sdk.moveapps_io import MoveAppsIo
-from tests.config.definitions import ROOT_DIR
 
 class MyTestCase(unittest.TestCase):
 
@@ -28,12 +28,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_find_stops(self):
+
         @dataclass
         class Testcase:
-            name: str
-            traj: mpd.Trajectory
-            config: dict
-            expect: tuple[bool, Point]
+            def __init__(self, name: str, traj: mpd.Trajectory, config: dict, expect: tuple[bool, Point]):
+                self.name = name
+                self.traj = traj
+                self.config = config
+                self.expect = expect
+
         default_config = {
             "stop_duration": 12,
             "distance_tolerance": 100,
@@ -46,11 +49,11 @@ class MyTestCase(unittest.TestCase):
                 {'geometry': Point(2, 2), 't': datetime(2023,1,1,7,0,0)},
                 {'geometry': Point(2, 2), 't': datetime(2023, 1, 1, 13, 0, 0)},
                 {'geometry': Point(2, 2), 't': datetime(2023, 1, 1, 20, 0, 0)}
-                ]).set_index('t'), crs='WSG84'), 1),
+                ]).set_index('t'), crs=4326), 1),
                 config=default_config, 
-                expect=[True, Point(1, 1)])
+                expect=[True, Point(2, 2)])
         ]
         for test in testcases:
-            stationary, location = self.sut.find_stops(test["traj"], test["config"])
-            self.assertEqual(stationary, test["expect"][0])
-            self.assertEqual(location, test["expect"][1])
+            stationary, location = self.sut.find_stops(test.traj, test.config)
+            self.assertEqual(stationary, test.expect[0])
+            self.assertEqual(location, test.expect[1])
